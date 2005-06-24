d208ab0c66bb5cdd7e91c2d2b1d1f9a7351e9a0d
Andrew Miner <asminer@iastate.edu>
2005-06-24 21:45:17 +0000

    First try
    git-svn-id: https://repo.cs.iastate.edu/svn/Smart/trunk@418 a175ec86-2bff-0310-88ad-935c21b1773c

--- Version2/Source/Templates/hash.h
+++ Version2/Source/Templates/hash.h
@@ -2,7 +2,15 @@
 // $Id$
 
 /*
-	Hash table class.
+	Hash table template class.
+	
+	Passed objects must have a public "next" member; 
+	i.e., you must make the template object the hash table nodes.
+
+	Objects must have a method "Hash(int m)"; 
+	this is the hashing function for a table of size m (m always prime).
+
+	Also, the operator == must work correctly on the objects.
 */
 
 #ifndef HASH_H
@@ -40,6 +48,151 @@ const int hash_sizes[num_hash_sizes] = {
    536870879,
   1073741789,
   2147483647
+};
+
+
+template <class DATA>
+class HashTable {
+protected:
+  int size_index;
+  int num_entries;
+  DATA** table;
+public:
+  HashTable(Manager <DATA> *nh) {
+    size_index = num_entries = 0;
+    nodeheap = nh;
+    table = NULL;
+  }
+  ~HashTable() {
+    // Will not delete entries.
+    free(table);
+  }
+  inline int Size() const {
+    CHECK_RANGE(0, size_index, num_hash_sizes);
+    return hash_sizes[size_index];
+  }
+  /// Empty the hash table into a list; returns the list.
+  DATA* ConvertToList() {
+    int i;
+    DATA* front = NULL;
+    for (i=0; i<Size(); i++) {
+      while (table[i]) {
+	DATA* foo = table[i]->next;
+	table[i]->next = front;
+	front = table[i];
+	table[i] = foo;
+	num_entries--;
+      } // while
+      if (num_entries <= 0) break;
+    } // for i
+    return front;
+  };
+  /// A series of inserts; doesn't check for duplicates or expand.
+  void BuildFromList(DATA* front) {
+    while (front) {
+      DATA* ptr = front;
+      front = front->next;
+      int h = ptr->Hash(Size());
+      ptr->next = table[h];
+      table[h] = ptr;
+      num_entries++;
+    }
+  }
+
+  void Expand() {
+    if (size_index+1 >= num_hash_sizes) return;
+    DATA* ptr = ConvertToList();
+    int os = Size();
+    size_index++;
+    table = (DATA**) realloc(table, sizeof(DATA*) * Size());
+    if (Size() && (NULL==table)) OutOfMemoryError("Hash table resize");
+    for (int i=os; i<Size(); i++) table[i] = NULL;
+    BuildFromList(ptr);
+  }
+
+  void Shrink() {
+    if (size_index==0) return;
+    DATA* ptr = ConvertToList();
+    size_index--;
+    table = (DATA**) realloc(table, sizeof(DATA*) * Size());
+    if (Size() && (NULL==table)) OutOfMemoryError("Hash table resize");
+    BuildFromList(ptr);
+  }
+
+  /** If table contains key, move it to the front of the list.
+      Otherwise, do nothing.
+      Returns ptr to the key if found, null otherwise.
+  */
+  DATA* Find(DATA* key) {
+    int h = key->Hash(Size());
+    DATA* parent = NULL;
+    DATA* ptr;
+    for (ptr = table[h]; ptr; ptr = ptr->next) {
+      if (*ptr == *key) break;
+      parent = ptr;
+    }
+    if (ptr) {
+      // remove from current spot
+      if (parent) parent->next = ptr->next;
+      else table[h] = ptr->next;
+      // move to front
+      ptr->next = table[h];
+      table[h] = ptr;
+    }
+    return ptr;
+  }
+
+  /** If table contains key, remove it and return it.
+      Otherwise, return NULL.
+  */
+  DATA* Remove(DATA* key) {
+    int h = key->Hash(Size());
+    DATA* parent = NULL;
+    DATA* ptr;
+    for (ptr = table[h]; ptr; ptr = ptr->next) {
+      if (*ptr == *key) break;
+      parent = ptr;
+    }
+    if (ptr) {
+      // remove from current spot
+      if (parent) parent->next = ptr->next;
+      else table[h] = ptr->next;
+      num_entries--;
+      if (num_entries < hash_sizes[size_index-1]) Shrink();
+    }
+    return ptr;
+  }
+
+
+  /** If table contains key, move it to the front of the list.
+      Otherwise, add key to the front of the list.
+      Returns the (new) front of the list.
+  */
+  DATA* Insert(DATA* key) {
+    if (num_entries >= hash_sizes[size_index+2]) Expand();
+    int h = key->Hash(Size());
+    DATA* parent = NULL;
+    DATA* ptr;
+    for (ptr = table[h]; ptr; ptr = ptr->next) {
+      if (*ptr == *key) break;
+      parent = ptr;
+    }
+    if (ptr) {
+      // remove from current spot
+      if (parent) parent->next = ptr->next;
+      else table[h] = ptr->next;
+      // move to front
+      ptr->next = table[h];
+      table[h] = ptr;
+    } else {
+      // add key in front
+      key->next = table[h];
+      table[h] = key;
+      num_entries++;
+    }
+    return table[h];
+  }
+
 };
 
 #endif
