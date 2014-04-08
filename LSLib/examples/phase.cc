55ba1fa5c9addf894d9cf53d1ebc2eb42fb1728d
Andrew Miner <asminer@iastate.edu>
2014-04-08 21:24:38 +0000

    Added example application for vector-matrix multiply: discrete phase-type distributions
    git-svn-id: https://repo.cs.iastate.edu/svn/SmartLibs/trunk@533 8e331fe6-4136-0410-8f16-cd66b33af5df

---
+++ LSLib/examples/phase.cc
@@ -0,0 +1,246 @@
+
+// $Id$
+
+#include "lslib.h"
+#include "timers.h"
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// #define DEBUG_IO
+
+int lineno;
+
+int Usage(const char* name)
+{
+  cerr << "Usage: " << name << " [file]\n\n";
+  cerr << "Reads a discrete phase type random variable from a file (or stdin)\n";
+  cerr << "(an initial vector and a matrix), and computes its distribution\n";
+  cerr << "(the pdf) using a series of vector-matrix multiplications and\n";
+  cerr << "displaying the last element.\n\n";
+  cerr << "File format (whitespace ignored, # to ignore rest of line):\n";
+  cerr << "\tMatrix dimension\n";
+  cerr << "\t[v, v, v, v] # Initial vector\n";
+  cerr << "\tNumber of matrix edges\n";
+  cerr << "\trow, col, value;\n";
+  cerr << "\trow, col, value;\n";
+  cerr << "\t...\n";
+  cerr << "\t(matrix edges should be in order)\n\n";
+  return 0;
+}
+
+void ignoreToEol(istream &s)
+{
+  for (;;) {
+    char c = s.get();
+    if ('\n' == c) {
+      lineno++;
+      return;
+    }
+    if (!s) return;
+  }
+}
+
+void skipUntil(istream &s, const char* expected, const char* error)
+{
+  for (;;) {
+    char c = s.get();
+    if (' ' == c) continue;
+    if ('\t' == c) continue;
+    if ('\n' == c) {
+      lineno++;
+      continue;
+    }
+    if ('\r' == c) continue;
+    if ('#' == c) {
+      ignoreToEol(s);
+      continue;
+    }
+    for (int i=0; expected[i]; i++) {
+      if (expected[i] == c) {
+        s.unget();
+        return;
+      }
+    }
+    // Bad character
+    throw error;
+  }
+}
+
+long readInt(istream &s)
+{
+  skipUntil(s, "0123456789", "Expected integer");
+  long N;
+  s >> N;
+  return N;
+}
+
+double readReal(istream &s)
+{
+  skipUntil(s, "0123456789", "Expected real");
+  double D;
+  s >> D;
+  return D;
+}
+
+void readLbrak(istream &s)
+{
+  skipUntil(s, "[", "Expecting `['");
+  s.get();
+}
+
+void readRbrak(istream &s)
+{
+  skipUntil(s, "]", "Expecting `]'");
+  s.get();
+}
+
+void readComma(istream &s)
+{
+  skipUntil(s, ",", "Expecting `,`");
+  s.get();
+}
+
+void readSemi(istream &s)
+{
+  skipUntil(s, ";", "Expecting `;`");
+  s.get();
+}
+
+template <typename T>
+inline void showVector(ostream &s, const char* name, const T *v, long n)
+{
+  s << name << ": [" << v[0];
+  for (int i=1; i<n; i++) s << ", " << v[i];
+  s << "]\n";
+}
+
+void parseInput(istream &s, LS_Matrix &A, double* &initial)
+{
+  lineno = 1;
+#ifdef DEBUG_IO
+  cerr << "Reading matrix dimension\n";
+#endif
+  long N = readInt(s);
+#ifdef DEBUG_IO
+  cerr << "Reading initial vector of size " << N << "\n";
+#endif
+  initial = new double[N];
+  readLbrak(s);
+  initial[0] = readReal(s);
+  for (long i=1; i<N; i++) {
+    readComma(s);
+    initial[i] = readReal(s);
+  }
+  readRbrak(s);
+#ifdef DEBUG_IO
+  cerr << "Got initial vector\n";
+  cerr << "Reading #matrix edges\n";
+#endif
+  long E = readInt(s);
+#ifdef DEBUG_IO
+  cerr << "Reading " << E << " edges\n";
+#endif
+  long lastindex = -1;
+  //
+  // Set up A
+  //
+  A.is_transposed = true;
+  A.start = 0;
+  A.stop = N;
+  long* rp = new long[N+1];
+  long* ci = new long[E];
+  float* fv = new float[E];
+  A.rowptr = rp;
+  A.colindex = ci;
+  A.f_value = fv;
+  A.d_value = 0;
+  A.f_one_over_diag = 0;
+  A.d_one_over_diag = 0;
+  // 
+  // Read the actual elements
+  //
+  for (long e=0; e<E; e++) {
+    long r = readInt(s);
+    readComma(s);
+    long c = readInt(s);
+    readComma(s);
+    double v = readReal(s);
+    readSemi(s);
+
+    if (r<0 || r>=N) throw "Row out of range";
+    if (c<0 || c>=N) throw "Column out of range";
+
+    if (r < lastindex) throw "Rows not in order";
+#ifdef DEBUG_IO
+    cerr << "  edge " << r << ", " << c << ", " << v << "\n";
+#endif
+    // update row pointers, if row changed
+    while (lastindex<r) {
+      ++lastindex;
+      rp[lastindex] = e;
+    }
+    ci[e] = c;
+    fv[e] = v;
+  } // for e
+  // And, get the final row pointers set
+  while (lastindex<N) {
+    ++lastindex;
+    rp[lastindex] = E;
+  }
+#ifdef DEBUG_IO
+  cerr << "Done reading matrix\n";
+  showVector(cerr, "rp", rp, N+1);
+  showVector(cerr, "ci", ci, E);
+  showVector(cerr, "fv", fv, E);
+#endif
+}
+
+inline void clearVector(double* x, int n)
+{
+  for (int i=0; i<n; i++) x[i] = 0;
+}
+
+int main(int argc, char** argv)
+{
+  if (argc > 2) return 1+Usage(argv[0]);
+
+  LS_Matrix A;
+  double* x;
+
+  try {
+    if (2 == argc) {
+      ifstream inf(argv[1]);
+      if (!inf) {
+        cerr << "Couldn't open file " << argv[1] << "\n";
+        return 2;
+      }
+      parseInput(inf, A, x);
+    } else {
+      parseInput(cin, A, x);
+    }
+  }
+  catch (const char* err) {
+    cerr << "Error near line " << lineno << ": " << err << "\n";
+    return 1;
+  }
+
+  double acc = 0.0;
+  double* y = new double[A.stop];
+
+  cout << "#Time Prob\n";
+  for (int t=0; ; t++) {
+    cout << t << "  " << x[A.stop-1] << "\n";
+    acc += x[A.stop-1];
+    if (1.0 - acc < 1e-6) break;
+    clearVector(y, A.stop);
+    A.VectorMatrixMultiply(y, x);
+    double* t = x;
+    x = y;
+    y = t;
+  }
+
+  return 0;
+}
