7cec86ac5e58da559fe213d0766af321e7c33979
Andrew Miner <asminer@iastate.edu>
2014-05-22 18:01:32 +0000

    Allowing sparse vectors in statevect module now :)
    git-svn-id: https://repo.cs.iastate.edu/svn/Smart/trunk@1425 a175ec86-2bff-0310-88ad-935c21b1773c

--- src/Modules/statevects.h
+++ src/Modules/statevects.h
@@ -13,6 +13,7 @@
 class exprman;
 class symbol_table;
 class stochastic_lldsm;
+class stateset;
 
 // ******************************************************************
 // *                                                                *
@@ -20,36 +21,70 @@ class stochastic_lldsm;
 // *                                                                *
 // ******************************************************************
 
-/// Explicit only (at least for now)
+/**
+    A vector of reals, per state.
+    For now - explicit storage only.
+    Vectors are stored either in full or sparsely.
+*/
 class statevect : public shared_object {
+protected:
   const stochastic_lldsm* parent;
-  double* vect;
-  long numStates;
+  long* indexes;    // If null, then we're stored as truncated full
+  double* vect;     // Entries
+  long vectsize;    // Dimension of vect[] and indexes[] arrays.
 public:
+  /**
+    Create a statevect by copying from an ordinary vector.
+    Will be stored sparsely or truncated full (whichever is more compact).
+  */
   statevect(const stochastic_lldsm* p, const double* d, long N);
-  statevect(const stochastic_lldsm* p, double* d, long N, bool own = false);
+  /**
+    Create a statevect, assuming the user has built it correctly.
+    I.e., either I is null and D is a full vector,
+    or I is a list of indexes for elements in D (sparse).
+  */
+  statevect(const stochastic_lldsm* p, long* I, double* D, long N);
   virtual ~statevect();
 
-  inline double read(long i) const {
+public:
+  inline bool isSparse() const {
+    return indexes;
+  }
+  inline double readFull(long i) const {
+    DCASSERT(0==indexes);
     DCASSERT(vect);
-    CHECK_RANGE(0, i, numStates);
+    CHECK_RANGE(0, i, vectsize);
     return vect[i];
   }
-  inline long size() const {
-    return numStates;
+  inline long readSparseIndex(long z) const {
+    DCASSERT(indexes);
+    CHECK_RANGE(0, z, vectsize);
+    return indexes[z];
   }
-public:
+  inline double readSparseValue(long z) const {
+    DCASSERT(vect);
+    CHECK_RANGE(0, z, vectsize);
+    return vect[z];
+  }
+  inline long size() const {
+    return vectsize;
+  }
+
   inline const stochastic_lldsm* getParent() const { return parent; }
 
   // required for shared_object
   virtual bool Print(OutputStream &s, int width) const;
   virtual bool Equals(const shared_object *o) const;
+public:
+  /// this[i] = (i in e) ? (sv[i]) : 0, for all i.
+  void copyRestricted(const statevect* sv, const intset* e);
 private:
   static int display_style;
   static const int FULL = 0;
   static const int SINDEX = 1;
   static const int SSTATE = 2;
   friend void InitStatevects(exprman* em, symbol_table* st);
+  friend class statevect_printer;
 };
 
 
@@ -62,9 +97,10 @@ private:
 class statedist : public statevect {
   public:
     statedist(const stochastic_lldsm *p, const double *d, long N);
-    statedist(const stochastic_lldsm* p, double* d, long N, bool own = false);
+    statedist(const stochastic_lldsm* p, long* I, double* D, long N);
 
-  // ANY difference?
+    /// Normalize vector if possible; returns original sum of elements.
+    double normalize();
 };
 
 
@@ -77,7 +113,7 @@ class statedist : public statevect {
 class stateprobs : public statevect {
   public:
     stateprobs(const stochastic_lldsm *p, const double *d, long N);
-    stateprobs(const stochastic_lldsm* p, double* d, long N, bool own = false);
+    stateprobs(const stochastic_lldsm* p, long* I, double* D, long N);
 
   // ANY difference?
 };
@@ -92,7 +128,7 @@ class stateprobs : public statevect {
 class statemsrs : public statevect {
   public:
     statemsrs(const stochastic_lldsm *p, const double *d, long N);
-    statemsrs(const stochastic_lldsm* p, double* d, long N, bool own = false);
+    statemsrs(const stochastic_lldsm* p, long* I, double* D, long N);
 
   // ANY difference?
 };
