b9d782a83d252d8c10d279a45b651de100b27c19
Andrew Miner <asminer@iastate.edu>
2016-01-05 15:37:13 +0000

    Finished (? draft of) meddly_ssets. Next step - clobber the ctl engines.
    git-svn-id: https://repo.cs.iastate.edu/svn/Smart/branches/asm-rss@1512 a175ec86-2bff-0310-88ad-935c21b1773c

--- src/Modules/meddly_ssets.cc
+++ src/Modules/meddly_ssets.cc
@@ -2,9 +2,9 @@
 // $Id$
 
 #include "../ExprLib/exprman.h"
+#include "../ExprLib/mod_vars.h"
 #include "../Formlsms/graph_llm.h"
 #include "glue_meddly.h"
-
 #include "meddly_ssets.h"
 
 #ifdef NEW_STATESETS
@@ -101,6 +101,57 @@ bool meddly_stateset::Intersect(const expr* c, const char* op, const stateset* x
   states = ans;
   return true;
   // TBD - use a try/catch block to check for errors
+}
+
+void meddly_stateset::getCardinality(long &card) const
+{
+  mdd_wrap->getCardinality(states, card);
+}
+
+void meddly_stateset::getCardinality(result &x) const
+{
+  mdd_wrap->getCardinality(states, x);
+}
+
+bool meddly_stateset::Print(OutputStream &s, int) const
+{
+  // TBD - option for printing indexes instead?
+
+  const hldsm* hm = getGrandparent();
+  DCASSERT(hm);
+
+  shared_state* st = new shared_state(hm);
+  const int* mt = mdd_wrap->firstMinterm(states);
+  s.Put('{');
+  bool comma = false;
+  while (mt) {
+    if (comma)  s << ", ";
+    else        comma = true;
+    mdd_wrap->minterm2state(mt, st);
+    hm->showState(s, st);
+    mt = mdd_wrap->nextMinterm(states);
+  }
+  s.Put('}');
+  Delete(st);
+  return true;
+}
+
+bool meddly_stateset::Equals(const shared_object *o) const
+{
+  const meddly_stateset* ms = dynamic_cast <const meddly_stateset*> (o);
+  if (0==ms) return false;
+
+  DCASSERT(vars);
+  if (!vars->Equals(ms->vars)) return false;
+
+  DCASSERT(mdd_wrap);
+  if (!mdd_wrap->Equals(ms->mdd_wrap)) return false;
+
+  if (states) {
+    return states->Equals(ms->states);
+  } else {
+    return 0==ms->states;
+  }
 }
 
 #endif
