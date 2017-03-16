bfd2c33ec1eaa11fdf5aafefb6f995155b0d17a4
Andrew Miner <asminer@iastate.edu>
2017-03-16 16:34:27 +0000

    Cleaned out old code
    git-svn-id: https://repo.cs.iastate.edu/svn/Smart/branches/asm-ctl@1606 a175ec86-2bff-0310-88ad-935c21b1773c

--- src/Formlsms/ctl_msr.h
+++ src/Formlsms/ctl_msr.h
@@ -4,18 +4,5 @@
 #ifndef CTL_MSR_H
 #define CTL_MSR_H
 
-#include "../include/list.h"
-
-class symbol_table;
-class exprman;
-class msr_func;
-
-/** 
-    Initialize CTL measure functions.
-      @param  st  Symbol table, for help topic(s), or any "global" functions.
-      @param  em  Expression manager.
-      @param  L   List of measures we should add to.
-*/
-// void InitCTLMeasureFuncs(symbol_table* st, exprman* em, List <msr_func> *L);
 
 #endif
