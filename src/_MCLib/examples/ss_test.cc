1c04ec1422c6632cc507a74fc2c8e1ec515943e1
Andrew Miner <asminer@iastate.edu>
2017-06-14 14:42:14 -0500

    Added test program for steady state computation.

--- src/_MCLib/examples/ss_test.cc
+++ src/_MCLib/examples/ss_test.cc
@@ -1,6 +1,6 @@
 
 /*
-    Tests for TTA computation and first hitting probabilities.
+    Tests for "steady state" computation
 */
 
 #include <iostream>
@@ -17,101 +17,30 @@ typedef struct { long from; long to; double rate; } edge;
 // ==============================> Graph 1 <==============================
 
 /*
-  Absorbing chain from lecture notes.
-    States 0,1,2 are transient.
-    States 3,4,5 are absorbing.
+  Land of oz.
 */
 const edge graph1[] = {
-  {0, 1, 2},  {0, 3, 1},
-  {1, 2, 3},  {1, 4, 1},
-  {2, 0, 4},  {2, 5, 1},
+  {0, 0, 2},  {0, 1, 1},  {0, 2, 1},
+  {1, 0, 1},              {1, 2, 1},
+  {2, 0, 1},  {2, 1, 1},  {2, 2, 2},
   // End 
   {-1, -1, -1}
 };
 
 const bool discrete1 = true;
-const long num_nodes1 = 6;
+const long num_nodes1 = 3;
 
-const double init1[] = {1.0/3.0, 0, 1.0/3.0, 1.0/3.0, 0, 0};
-const double tta1[] =  {1, 2.0/3.0, 5.0/6.0,    2.0/3.0, 1.0/6.0, 1.0/6.0};
+const double init1[] = {1, 0, 0}; // doesn't matter
+const double pinfinity1[] =  {2.0/5.0, 1.0/5.0, 2.0/5.0};
 
 // ==============================> Graph 2 <==============================
-
-/*
-  Absorbing chain from lecture notes, converted to CTMC.
-    States 0,1,2 are transient.
-    States 3,4,5 are absorbing.
-*/
-const edge graph2[] = {
-  {0, 1, 2},  {0, 3, 1},    // Same as DTMC except row multiplied by 3
-  {1, 2, 3},  {1, 4, 1},    // Same as DTMC except row multiplied by 4
-  {2, 0, 4},  {2, 5, 1},    // Same as DTMC except row multiplied by 5
-  // End 
-  {-1, -1, -1}
-};
-
-const bool discrete2 = false;
-const long num_nodes2 = 6;
-
-const double init2[] = {1.0/3.0, 0, 1.0/3.0, 1.0/3.0, 0, 0};
-const double tta2[] =  {1.0/3.0, 1.0/6.0, 1.0/6.0,    2.0/3.0, 1.0/6.0, 1.0/6.0};
-// Adjust tta times: divide first by 3, second by 4, third by 5.
-
-// ==============================> Graph 3 <==============================
-
-/*
-  Absorbing chain from lecture notes, converted to CTMC.
-    States 0,1,2 are transient.
-    States 3,4,5 are absorbing.
-*/
-const edge graph3[] = {
-  {0, 1, 2.0/3.0},  {0, 3, 1.0/3.0},    // Same as DTMC 
-  {1, 2, 1.0/4.0},  {1, 4, 1.0/12.0},   // Same as DTMC except row divided by 3
-  {2, 0, 2.0/15.0}, {2, 5, 1.0/30.0},   // Same as DTMC except row divided by 6
-  // End 
-  {-1, -1, -1}
-};
-
-const bool discrete3 = false;
-const long num_nodes3 = 6;
-
-const double init3[] = {1.0/3.0, 0, 1.0/3.0, 1.0/3.0, 0, 0};
-const double tta3[] =  {1.0, 2.0, 5.0,    2.0/3.0, 1.0/6.0, 1.0/6.0};
-// Adjust tta times: first ok, multiply second by 3, multiply third by 6
-
-// ==============================> Graph 4 <==============================
-
-/*
-  Absorbing chain from lecture notes, converted to CTMC.
-    States 0,1,2 are transient.
-    States 3,4,5 are absorbing.
-*/
-const edge graph4[] = {
-  //
-  // Use DTMC and multiply probabilities by 15
-  //
-  {0, 1, 10},         {0, 3, 5},
-  {1, 2, 45.0/4.0},   {1, 4, 15.0/4.0},  
-  {2, 0, 12},         {2, 5, 3},
-  // End 
-  {-1, -1, -1}
-};
-
-const bool discrete4 = false;
-const long num_nodes4 = 6;
-
-const double init4[] = {1.0/3.0, 0, 1.0/3.0, 1.0/3.0, 0, 0};
-const double tta4[] =  {1.0/15.0, 2.0/45.0, 1.0/18.0,    2.0/3.0, 1.0/6.0, 1.0/6.0};
-// Adjust tta times: divide all by 15
-
-// ==============================> Graph 5 <==============================
 
 /*
   The "university" DTMC.
     States 0,1,2,3 are transient.
     States 4,5 are absorbing.
 */
-const edge graph5[] = {
+const edge graph2[] = {
   {0, 0, 1},  // repeat fr prob 1/10
   {0, 5, 1},  // flunk out fr prob 1/10
   {0, 1, 8},  // pass fr prob 8/10
@@ -128,41 +57,182 @@ const edge graph5[] = {
   {-1, -1, -1}
 };
 
-const bool discrete5 = true;
-const long num_nodes5 = 6;
+const bool discrete2 = true;
+const long num_nodes2 = 6;
 
-const double init5[] = {0.7, 0.3, 0, 0, 0, 0};
-const double tta5[] =  {7.0/9.0, 83.0/72.0, 581.0/576.0, 4067.0/5184.0,     4067.0/6480.0, 2413.0/6480.0};
+const double init2[] = {0.7, 0.3, 0, 0, 0, 0};
+const double pinfinity2[] =  {0, 0, 0, 0, 4067.0/6480.0, 2413.0/6480.0};
 
-// ==============================> Graph 6 <==============================
+// ==============================> Graph 3 <==============================
 
 /*
-  The "university" DTMC, with all rates divided by 9.
-    States 0,1,2,3 are transient.
-    States 4,5 are absorbing.
+  Simple two-state CTMC.
 */
-const edge graph6[] = {
-  {0, 0, 1.0/90.0},  // repeat fr prob 1/10
-  {0, 5, 1.0/90.0},  // flunk out fr prob 1/10
-  {0, 1, 8.0/90.0},  // pass fr prob 8/10
-  {1, 1, 2.0/90.0},  // repeat soph prob 2/10
-  {1, 5, 1.0/90.0},  // flunk out soph prob 1/10
-  {1, 2, 7.0/90.0},  // pass soph prob 7/10
-  {2, 2, 2.0/90.0},  // repeat jr prob 2/10
-  {2, 5, 1.0/90.0},  // flunk out jr prob 1/10
-  {2, 3, 7.0/90.0},  // pass jr prob 7/10
-  {3, 3, 1.0/90.0},  // repeat sr prob 1/10
-  {3, 5, 1.0/90.0},  // flunk out sr prob 1/10
-  {3, 4, 8.0/90.0},  // pass sr prob 8/10
+const edge graph3[] = {
+  {0, 1, 0.75},
+  {1, 0, 1},
   // End 
   {-1, -1, -1}
 };
 
-const bool discrete6 = false;
-const long num_nodes6 = 6;
+const bool discrete3 = false;
+const long num_nodes3 = 2;
 
-const double init6[] = {0.7, 0.3, 0, 0, 0, 0};
-const double tta6[] =  {7.0, 83.0/8.0, 581.0/64.0, 4067.0/576.0,     4067.0/6480.0, 2413.0/6480.0};
+const double init3[] = {1, 0};
+const double pinfinity3[] =  { 1.0/1.75, 0.75/1.75};
+
+// ==============================> Graph 4 <==============================
+
+/*
+  Land of oz with selector state at beginning and absorbing state.
+*/
+const edge graph4[] = {
+  // Go to "Oz" with probability 5/8
+  {0, 2, 5},  {0, 1, 3},
+  // Land of oz portion
+  {2, 2, 2},  {2, 3, 1},  {2, 4, 1},
+  {3, 2, 1},              {3, 4, 1},
+  {4, 2, 1},  {4, 3, 1},  {4, 4, 2},
+  // End 
+  {-1, -1, -1}
+};
+
+const bool discrete4 = true;
+const long num_nodes4 = 5;
+
+const double init4[] = {1, 0, 0, 0, 0}; 
+const double pinfinity4[] =  {0,  3.0/8.0,   2.0/8.0, 1.0/8.0, 2.0/8.0};
+
+const double init4a[] = {0, 1, 0, 0, 0}; 
+const double pinfinity4a[] =  {0,  1,   0, 0, 0};
+
+const double init4b[] = {0, 0, 1, 0, 0}; 
+const double pinfinity4b[] =  {0,  0,   2.0/5.0, 1.0/5.0, 2.0/5.0};
+
+// ==============================> Graph 4 <==============================
+
+/*
+  FMS N=1 CTMC.
+*/
+const edge graph5[] = {
+	{0, 1, 0.333333}, {0, 2, 0.333333}, {0, 3, 0.333333},
+	{1, 4, 0.2}, {1, 5, 0.05}, {1, 6, 0.5}, {1, 7, 0.5},
+	{2, 6, 0.5}, {2, 8, 0.0666667}, {2, 9, 0.1}, {2, 10, 0.5},
+	{3, 7, 0.5}, {3, 10, 0.5}, {3, 11, 0.5},
+	{4, 0, 0.0166667}, {4, 12, 0.5}, {4, 13, 0.5},
+	{5, 14, 0.5}, {5, 15, 0.5},
+	{6, 12, 0.2}, {6, 14, 0.05}, {6, 16, 0.0666667}, {6, 17, 0.1}, {6, 18, 1},
+	{7, 13, 0.2}, {7, 15, 0.05}, {7, 18, 1}, {7, 19, 0.5},
+	{8, 16, 0.5}, {8, 20, 0.5},
+	{9, 0, 0.0166667}, {9, 17, 0.5}, {9, 21, 0.5},
+	{10, 18, 1}, {10, 20, 0.0666667}, {10, 21, 0.1},
+	{11, 0, 0.0166667}, {11, 19, 0.5}, {11, 22, 0.5},
+	{12, 2, 0.0166667}, {12, 23, 0.0666667}, {12, 24, 0.1}, {12, 25, 1},
+	{13, 3, 0.0166667}, {13, 25, 1}, {13, 26, 0.5},
+	{14, 27, 0.0666667}, {14, 28, 0.1}, {14, 29, 1},
+	{15, 29, 1}, {15, 30, 0.5},
+	{16, 23, 0.2}, {16, 27, 0.05}, {16, 31, 1},
+	{17, 1, 0.0166667}, {17, 24, 0.2}, {17, 28, 0.05}, {17, 32, 1},
+	{18, 25, 0.2}, {18, 29, 0.05}, {18, 31, 0.0666667}, {18, 32, 0.1},
+	{19, 1, 0.0166667}, {19, 26, 0.2}, {19, 30, 0.05}, {19, 33, 1},
+	{20, 31, 1}, {20, 34, 0.5},
+	{21, 3, 0.0166667}, {21, 32, 1}, {21, 35, 0.5},
+	{22, 2, 0.0166667}, {22, 33, 1}, {22, 34, 0.0666667}, {22, 35, 0.1},
+	{23, 8, 0.0166667}, {23, 36, 1},
+	{24, 4, 0.0166667}, {24, 9, 0.0166667}, {24, 37, 1},
+	{25, 10, 0.0166667}, {25, 36, 0.0666667}, {25, 37, 0.1},
+	{26, 4, 0.0166667}, {26, 11, 0.0166667}, {26, 38, 1},
+	{27, 39, 0.5}, {27, 40, 0.5},
+	{28, 5, 0.0166667}, {28, 41, 1},
+	{29, 40, 0.0666667}, {29, 41, 0.1},
+	{30, 5, 0.0166667}, {30, 42, 1},
+	{31, 36, 0.2}, {31, 40, 0.05}, {31, 43, 0.5},
+	{32, 7, 0.0166667}, {32, 37, 0.2}, {32, 41, 0.05}, {32, 44, 0.5},
+	{33, 6, 0.0166667}, {33, 38, 0.2}, {33, 42, 0.05}, {33, 43, 0.0666667}, {33, 44, 0.1},
+	{34, 8, 0.0166667}, {34, 43, 1},
+	{35, 9, 0.0166667}, {35, 11, 0.0166667}, {35, 44, 1},
+	{36, 20, 0.0166667}, {36, 45, 0.5},
+	{37, 13, 0.0166667}, {37, 21, 0.0166667}, {37, 46, 0.5},
+	{38, 12, 0.0166667}, {38, 22, 0.0166667}, {38, 45, 0.0666667}, {38, 46, 0.1},
+	{39, 47, 1}, {39, 48, 1},
+	{40, 48, 1}, {40, 49, 0.5},
+	{41, 15, 0.0166667}, {41, 50, 0.5},
+	{42, 14, 0.0166667}, {42, 49, 0.0666667}, {42, 50, 0.1},
+	{43, 16, 0.0166667}, {43, 45, 0.2}, {43, 49, 0.05},
+	{44, 17, 0.0166667}, {44, 19, 0.0166667}, {44, 46, 0.2}, {44, 50, 0.05},
+	{45, 23, 0.0166667}, {45, 34, 0.0166667},
+	{46, 24, 0.0166667}, {46, 26, 0.0166667}, {46, 35, 0.0166667},
+	{47, 0, 0.0166667}, {47, 51, 1},
+	{48, 51, 1}, {48, 52, 0.5},
+	{49, 27, 0.0166667}, {49, 52, 1},
+	{50, 28, 0.0166667}, {50, 30, 0.0166667},
+	{51, 3, 0.0166667}, {51, 53, 0.5},
+	{52, 39, 0.0166667}, {52, 53, 1},
+	{53, 11, 0.0166667}, {53, 47, 0.0166667},
+  {-1, -1, -1}
+};
+
+const bool discrete5 = false;
+const long num_nodes5 = 54;
+
+const double init5[] = {1, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
+const double pinfinity5[] =  {
+  8.80289789903105e-05,
+  4.12047186504873e-05,
+  4.79976446062514e-05,
+  7.84355735594038e-05,
+  7.225484795164e-05,
+  6.13936132538748e-05,
+  9.92147120577963e-05,
+  3.77638741763752e-05,
+  0.000270183469490893,
+  6.72591275804355e-05,
+  8.97068609000315e-05,
+  0.0026029196409182,
+  0.000273364089623626,
+  7.36815611807847e-05,
+  0.000194098284399786,
+  6.3417893228892e-05,
+  0.000564502572866163,
+  0.000160890413809861,
+  0.000544045047476306,
+  0.00116888306133098,
+  0.000273880030456362,
+  7.29695069907923e-05,
+  0.00132589032581936,
+  0.00796316126298326,
+  0.00185439422772452,
+  0.00248647915743128,
+  0.00205868734416382,
+  8.25763448868046e-05,
+  0.0017491623576754,
+  0.00170831020617031,
+  0.00181083325864178,
+  0.00116620309964201,
+  0.000375997062040278,
+  0.00575716878464422,
+  0.00805582481734845,
+  0.00196041934393682,
+  0.0161848384905256,
+  0.00408420246353817,
+  0.0160505981496407,
+  4.15125967315215e-05,
+  0.000142323827772277,
+  0.00375250276146759,
+  0.0114474055762991,
+  0.0338352627808454,
+  0.0096145908856935,
+  0.477884431694763,
+  0.111401363944213,
+  0.00253928970917687,
+  0.000122557621691344,
+  0.00248467451526003,
+  0.105051440056555,
+  0.00515196216677018,
+  0.00250421631347454,
+  0.152405623328974
+};
+
 
 // =======================================================================
 
@@ -348,7 +418,7 @@ void show_vector(const char* name, const double* x, long size)
 
 double diff_vector(const double* A, const double* B, long size)
 {
-  double rel_diff = 0.0;
+  double rel_diff = 0;
   for (long i=0; i<size; i++) {
     double d = A[i] - B[i];
     if (d<0) d*=-1;
@@ -361,7 +431,7 @@ double diff_vector(const double* A, const double* B, long size)
 // =======================================================================
 
 bool run_test(const char* name, const bool discrete, const edge graph[], 
-  const long num_nodes, const double init[], const double tta[])
+  const long num_nodes, const double init[], const double ss[])
 {
   cout << "Testing ";
   if (discrete) cout << "DTMC "; else cout << "CTMC ";
@@ -393,14 +463,14 @@ bool run_test(const char* name, const bool discrete, const edge graph[],
   LS_Output outf;
 
   //
-  // Solve TTA and first hitting probs
+  // Solve Infinite time probabilities
   //
   double* sold = new double[num_nodes];
   double* solf = new double[num_nodes];
 
   try {
-    MCd->computeFirstRecurrentProbs(p0, sold, opt, outd);
-    MCf->computeFirstRecurrentProbs(p0, solf, opt, outd);
+    MCd->computeInfinityDistribution(p0, sold, opt, outd);
+    MCf->computeInfinityDistribution(p0, solf, opt, outf);
   }
   catch (GraphLib::error e) {
     cout << "    Caught graph library error: ";
@@ -413,10 +483,10 @@ bool run_test(const char* name, const bool discrete, const edge graph[],
 
   show_vector("MCd solution vector", sold, num_nodes);
   show_vector("MCf solution vector", solf, num_nodes);
-  show_vector("Expected     vector", tta, num_nodes);
+  show_vector("Expected     vector", ss, num_nodes);
 
-  double diff_d = diff_vector(tta, sold, num_nodes);
-  double diff_f = diff_vector(tta, solf, num_nodes);
+  double diff_d = diff_vector(ss, sold, num_nodes);
+  double diff_f = diff_vector(ss, solf, num_nodes);
 
   cout << "  MCd relative difference: " << diff_d;
 
@@ -446,25 +516,27 @@ bool run_test(const char* name, const bool discrete, const edge graph[],
 
 int main()
 {
-  if (!run_test("abcdef dtmc", discrete1, graph1, num_nodes1, init1, tta1)) {
+  if (!run_test("Oz dtmc", discrete1, graph1, num_nodes1, init1, pinfinity1)) {
     return 1;
   }
-  if (!run_test("abcdef ctmc1", discrete2, graph2, num_nodes2, init2, tta2)) {
+  if (!run_test("University dtmc", discrete2, graph2, num_nodes2, init2, pinfinity2)) {
     return 1;
   }
-  if (!run_test("abcdef ctmc2", discrete3, graph3, num_nodes3, init3, tta3)) {
+  if (!run_test("2-state ctmc", discrete3, graph3, num_nodes3, init3, pinfinity3)) {
     return 1;
   }
-  if (!run_test("abcdef ctmc3", discrete4, graph4, num_nodes4, init4, tta4)) {
+  if (!run_test("Reducible dtmc", discrete4, graph4, num_nodes4, init4, pinfinity4)) {
     return 1;
   }
-  if (!run_test("univ dtmc", discrete5, graph5, num_nodes5, init5, tta5)) {
+  if (!run_test("Reducible dtmc", discrete4, graph4, num_nodes4, init4a, pinfinity4a)) {
     return 1;
   }
-  if (!run_test("univ ctmc", discrete6, graph6, num_nodes6, init6, tta6)) {
+  if (!run_test("Reducible dtmc", discrete4, graph4, num_nodes4, init4b, pinfinity4b)) {
     return 1;
   }
-
+  if (!run_test("FMS N=1 ctmc", discrete5, graph5, num_nodes5, init5, pinfinity5)) {
+    return 1;
+  }
   return 0;
 }
 
