3e2b760cd3ea68687835937bc77d4fca93dcccc1
Andrew Miner <asminer@iastate.edu>
2017-06-20 21:11:09 -0500

    Implemented continuous tta distro and test.

--- src/_MCLib/examples/dist_test.cc
+++ src/_MCLib/examples/dist_test.cc
@@ -4,6 +4,7 @@
 */
 
 #include <iostream>
+#include <math.h>
 
 #include "mcbuilder.h"
 
@@ -115,6 +116,43 @@ const double distro4[] = {0.25, 0.25, 0.25, -1};
 const double infty4 = 0.25;
 
 
+// ==============================> Graph 5 <==============================
+
+/*
+  Expo(2).
+*/
+const edge graph5[] = {
+  {0, 1, 2.0},
+  {-1, -1, -1}
+};
+
+const long num_nodes5 = 2;
+const long accept5 = 1;
+
+const double init5[] = {1, 0};
+const double zero5 = 0;
+const double infty5 = 0;
+
+double pdf5(double t) {
+  return 2.0 * exp(-2.0*t);
+}
+
+const double dts5[] = { 1.6, 0.8, 0.4, 0.2, 0.1, 0.08, 0.04, 0.02, 0.01, 0};
+
+// =======================================================================
+
+void show_distro(const char* name, double (*pdf)(double), 
+  double dt, long stop, const double zero, const double infty)
+{
+  cout << "  " << name << ": " << zero; 
+  for (long i=1; i<stop; i++) {
+    cout << ", " << pdf(i*dt);
+  }
+  if (infty) {
+    cout << ", 0, 0, ...; " << infty << ".";
+  }
+  cout << "\n";
+}
 
 // =======================================================================
 
@@ -130,7 +168,7 @@ void show_distro(const char* name, const double* x, const double infty)
 
 // =======================================================================
 
-void show_distro(const char* name, discrete_pdf &x)
+void show_distro(const char* name, const discrete_pdf &x)
 {
   cout << "  " << name << ": " << x.f(0);
   for (long i=1; i<=x.right_trunc(); i++) {
@@ -145,16 +183,37 @@ void show_distro(const char* name, discrete_pdf &x)
 
 // =======================================================================
 
-double diff_distro(const double* A, double inftyA, discrete_pdf &B)
+inline double reldiff(double targ, double val)
 {
-  double rel_diff = inftyA - B.f_infinity();
-  if (rel_diff<0) rel_diff *= -1;
-  if (inftyA) rel_diff /= inftyA;
+  double d = targ - val;
+  if (d<0) d*=-1;
+  if (targ) d /= targ;
+  return d;
+}
+
+// =======================================================================
+
+double diff_distro(const double* A, double inftyA, const discrete_pdf &B)
+{
+  double rel_diff = reldiff(inftyA, B.f_infinity());
 
   for (long i=0; A[i]>=0; i++) {
-    double d = A[i] - B.f(i);
-    if (d<0) d*=-1;
-    if (A[i]) d /= A[i];
+    double d = reldiff(A[i], B.f(i));
+    if (d > rel_diff) rel_diff = d;
+  }
+  return rel_diff;
+}
+
+// =======================================================================
+
+double diff_distro(double (*pdf)(double), double zeroA, double inftyA, const discrete_pdf &B, double dt)
+{
+  double rel_diff = reldiff(zeroA, B.f(0));
+  double d = reldiff(inftyA, B.f_infinity());
+  if (d > rel_diff) rel_diff = d;
+
+  for (long i=1; i<=B.right_trunc(); i++) {
+    d = reldiff(pdf(i*dt), B.f(i));
     if (d > rel_diff) rel_diff = d;
   }
   return rel_diff;
@@ -197,10 +256,14 @@ bool run_dtmc_test(const char* name, const edge graph[],
   // Compute distributions
   //
   discrete_pdf distd, distf;
+  double prec_d, prec_f;
 
   try {
     MCd->computeDiscreteDistTTA(opt, p0, accept, distd);
+    prec_d = opt.precision;
+
     MCf->computeDiscreteDistTTA(opt, p0, accept, distf);
+    prec_f = opt.precision;
   }
   catch (GraphLib::error e) {
     cout << "    Caught graph library error: ";
@@ -218,7 +281,9 @@ bool run_dtmc_test(const char* name, const edge graph[],
   //
 
   show_distro("MCd distribution", distd);
+  cout << "    precision: " << prec_d << "\n";
   show_distro("MCf distribution", distf);
+  cout << "    precision: " << prec_f << "\n";
   show_distro("Expected  distro", distro, infty);
 
   double diff_d = diff_distro(distro, infty, distd);
@@ -249,6 +314,106 @@ bool run_dtmc_test(const char* name, const edge graph[],
 
 // =======================================================================
 
+bool run_ctmc_test(const char* name, const edge graph[], 
+  const long num_nodes, const long accept, const double init[], 
+  const double q, const double dt,
+  double (*pdf)(double), const double zero, const double infty)
+{
+#ifdef VERBOSE
+  const bool verbose = true;
+#else
+  const bool verbose = false;
+#endif
+
+  cout << "Testing CTMC ";
+  cout << name << " with dt=" << dt << "\n";
+
+  Markov_chain* MCd = build_double(false, graph, num_nodes, verbose);
+  Markov_chain* MCf = build_float(false, graph, num_nodes, verbose);
+
+  //
+  // Set up p0
+  //
+  LS_Vector p0;
+  p0.index = 0;
+  p0.size = num_nodes;
+  p0.d_value = init;
+  p0.f_value = 0;
+
+  //
+  // Set up options
+  //
+  Markov_chain::CTMC_distribution_options opt;
+  opt.setMaxSize(1000);
+  opt.q = q;
+
+  //
+  // Compute distributions
+  //
+  discrete_pdf distd, distf;
+  double prec_d, prec_f;
+
+  try {
+    MCd->computeContinuousDistTTA(opt, p0, accept, dt, distd);
+    prec_d = opt.precision;
+
+    MCf->computeContinuousDistTTA(opt, p0, accept, dt, distf);
+    prec_f = opt.precision;
+  }
+  catch (GraphLib::error e) {
+    cout << "    Caught graph library error: ";
+    cout << e.getString() << "\n";
+    return false;
+  }
+  catch (MCLib::error e) {
+    cout << "    Caught Markov chain library error: ";
+    cout << e.getString() << "\n";
+    return false;
+  }
+
+  //
+  // Check results
+  //
+
+  double diff_d = diff_distro(pdf, zero, infty, distd, dt);
+  cout << "  MCd:\n";
+  cout << "    prec: " << prec_d << "\n";
+  cout << "    size: " << distd.right_trunc() << "\n";
+  cout << "    relative difference: " << diff_d;
+
+  if (diff_d < 1e-5) {
+    cout << " (OK)\n";
+  }
+  else {
+    cout << " too large!\n";
+    show_distro("MCd distribution", distd);
+    show_distro("Expected  distro", pdf, dt, distd.right_trunc()+1, zero, infty);
+    return false;
+  }
+
+  //
+
+  double diff_f = diff_distro(pdf, zero, infty, distf, dt);
+  cout << "  MCf:\n";
+  cout << "    prec: " << prec_f << "\n";
+  cout << "    size: " << distf.right_trunc() << "\n";
+  cout << "    relative difference: " << diff_f;
+
+  if (diff_f < 1e-5) {
+    cout << " (OK)\n";
+  }
+  else {
+    cout << " too large!\n";
+    show_distro("MCf distribution", distf);
+    show_distro("Expected  distro", pdf, dt, distf.right_trunc()+1, zero, infty);
+    return false;
+  }
+
+  return true;
+}
+
+// =======================================================================
+
 int main()
 {
   cout.precision(8);
@@ -267,6 +432,15 @@ int main()
   }
   if (!run_dtmc_test("0, 1, 2, or infinity", graph4, num_nodes4, accept4, init4, distro4, infty4)) {
     return 1;
+  }
+
+  //
+  // CTMC tests
+  //
+  for (long i=0; dts5[i]; i++) {
+    if (!run_ctmc_test("Expo(2)", graph5, num_nodes5, accept5, init5, 2, dts5[i], pdf5, zero5, infty5)) {
+      return 1;
+    }
   }
 
 
