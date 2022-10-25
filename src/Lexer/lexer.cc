75c081d5d7f67e69ef07bf45c39929252700b709
Andrew Miner <asminer@iastate.edu>
2022-10-24 22:15:18 -0500

    Warning messages

--- src/Lexer/lexer.cc
+++ src/Lexer/lexer.cc
@@ -636,6 +636,10 @@ void lexer::finish_attributed_token(token::type t)
     lookaheads[0].tokenID = t;
 
     if (text.is_truncated()) {
+        if (em->startWarning(lookaheads[0].where, text.get())) {
+            em->warn() << "Token too long; only keeping first " << MAX_LEXEME << " characters.";
+            em->stopIO();
+        }
         // TBD: warning message here!
     }
 }
