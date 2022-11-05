bfb931655b8e96467080821dfda547cc9a032320
Andrew Miner <asminer@iastate.edu>
2022-11-05 01:35:50 -0500

    More splitting

--- src/Options/options.cc
+++ src/Options/options.cc
@@ -2,6 +2,7 @@
 #include "../include/defines.h"
 #include "options.h"
 #include "opt_enum.h"
+#include "optman.h"
 #include "../Streams/streams.h"
 #include <stdlib.h>
 #include "../include/splay.h"
@@ -943,114 +944,5 @@ bool checklist_opt::isApropos(const doc_formatter* df, const char* keyword) cons
 option* MakeChecklistOption(const char* name, const char* doc)
 {
   return new checklist_opt(name, doc);
-}
-
-// **************************************************************************
-// *                                                                        *
-// *                         Option list management                         *
-// *                                                                        *
-// **************************************************************************
-
-option_manager::option_manager() { }
-option_manager::~option_manager() { }
-
-class option_heap : public option_manager {
-  SplayOfPointers <option> *optlist;
-  option** SortedOptions;
-  long NumSortedOptions;
-public:
-  option_heap();
-  virtual void AddOption(option *);
-  virtual void DoneAddingOptions();
-  virtual option* FindOption(const char* name) const;
-  virtual long NumOptions() const {
-    return NumSortedOptions;
-  }
-  virtual option* GetOptionNumber(long i) const {
-    if (i>=NumSortedOptions) return 0;
-    if (0==SortedOptions) return 0;
-    return SortedOptions[i];
-  }
-  virtual void DocumentOptions(doc_formatter* df, const char* keyword) const;
-  virtual void ListOptions(doc_formatter* df) const;
-};
-
-
-option_heap::option_heap() : option_manager()
-{
-  optlist = new SplayOfPointers <option> (16, 0);
-  SortedOptions = 0;
-  NumSortedOptions = 0;
-}
-
-void option_heap::AddOption(option *o)
-{
-  DCASSERT(0==SortedOptions);
-  optlist->Insert(o);
-}
-
-void option_heap::DoneAddingOptions()
-{
-  DCASSERT(0==SortedOptions);
-  NumSortedOptions = optlist->NumElements();
-  SortedOptions = new option*[NumSortedOptions];
-  optlist->CopyToArray(SortedOptions);
-  delete optlist;
-  optlist = 0;
-  for (int i=0; i<NumSortedOptions; i++) SortedOptions[i]->Finish();
-}
-
-option* option_heap::FindOption(const char* name) const
-{
-  if (optlist) {
-    option* find = optlist->Find(name);
-    return find;
-  }
-  DCASSERT(SortedOptions);
-  // binary search
-  int low = 0;
-  int high = NumSortedOptions;
-  while (low < high) {
-    int mid = (low+high)/2;
-    int cmp = strcmp(SortedOptions[mid]->Name(), name);
-    if (0==cmp) return SortedOptions[mid];
-    if (cmp>0) {
-      high = mid;
-    } else {
-      low = mid+1;
-    }
-  }
-  // not found
-  return 0;
-}
-
-void option_heap::DocumentOptions(doc_formatter* df, const char* keyword) const
-{
-  if (0==df)  return;
-  DCASSERT(SortedOptions);
-  for (int i=0; i<NumSortedOptions; i++)
-    if (SortedOptions[i]->isApropos(df, keyword)) {
-      df->Out() << "\n";
-      SortedOptions[i]->PrintDocs(df, keyword);
-    }
-}
-
-void option_heap::ListOptions(doc_formatter* df) const
-{
-  if (0==df)  return;
-  DCASSERT(SortedOptions);
-  for (int i=0; i<NumSortedOptions; i++) {
-#ifndef DEVELOPMENT_CODE
-    if (SortedOptions[i]->IsUndocumented())  continue;
-#endif
-    SortedOptions[i]->ShowCurrent(df->Out());
-    df->Out() << "\n";
-  }
-}
-
-
-option_manager* MakeOptionManager()
-{
-  return new option_heap;
 }
 
--- src/Options/options.h
+++ src/Options/options.h
@@ -12,6 +12,7 @@ class doc_formatter;   // defined in streams.h
 class option_manager;
 
 #include "opt_enum.h"
+#include "optman.h"
 
 // **************************************************************************
 // *                            option interface                            *
@@ -198,49 +199,6 @@ public:
   virtual bool isApropos(const doc_formatter* df, const char* keyword) const;
 };
 
-// **************************************************************************
-// *                        option_manager interface                        *
-// **************************************************************************
-
-/** Option manager (abstract) class.
-    A centralized collection of options, with operations for
-    addition of options (during initialization) and
-    retrieval of optiions (during computation).
-*/
-class option_manager {
-public:
-  option_manager();
-  virtual ~option_manager();
-  /// Add a new option to this collection.
-  virtual void AddOption(option *) = 0;
-  /** Called when initialization is complete.
-      After this is called, no new options may be added.
-  */
-  virtual void DoneAddingOptions() = 0;
-  /** Find an option with matching name.
-        @param  name  Name of the desired option.
-        @return The matching option, or NULL if none present with \a name.
-  */
-  virtual option* FindOption(const char* name) const = 0;
-  /// Total number of options.
-  virtual long NumOptions() const = 0;
-  /** Retrieve an option by index.
-      This is useful for enumerating all options.
-        @param  i  Index of option to retrieve.
-        @return The ith option.
-  */
-  virtual option* GetOptionNumber(long i) const = 0;
-
-  /** For online help and documentation.
-      Show documentation of all options matching the given keyword.
-  */
-  virtual void DocumentOptions(doc_formatter* df, const char* keyword) const = 0;
-
-  /** For online help and documentation.
-      List all options, with their current settings.
-  */
-  virtual void ListOptions(doc_formatter* df) const = 0;
-};
 
 // **************************************************************************
 // *                            Global interface                            *
@@ -351,14 +309,6 @@ option* MakeRealOption(const char* name, const char* doc, double &link,
       @return  A new option, or NULL on error.
 */
 option* MakeStringOption(const char* name, const char* doc, char* &link);
-
-
-
-/** Make a new option manager.
-      @return A new manager, or NULL on error.
-*/
-option_manager* MakeOptionManager();
-
 
 
 #endif
