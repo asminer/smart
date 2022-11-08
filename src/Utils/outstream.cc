292a45862622ed1db4dcc8407eda999c9c0d1859
Andrew Miner <asminer@iastate.edu>
2022-11-07 21:49:36 -0600

    Working on new output Stream

--- src/Utils/outstream.cc
+++ src/Utils/outstream.cc
@@ -1,5 +1,8 @@
 
 #include "outstream.h"
+#include "strings.h"
+
+#include <iomanip>
 
 outputStream::outputStream(std::ostream &_deflt) : deflt(_deflt)
 {
@@ -20,6 +23,108 @@ void outputStream::defaultOutput()
 {
     if (fout.is_open()) {
         fout.close();
+    }
+}
+
+void outputStream::PutMemoryCount(size_t bytes, int prec)
+{
+    const double kilo = bytes / 1024.0;
+    const double mega = kilo / 1024.0;
+    const double giga = mega / 1024.0;
+    const double tera = giga / 1024.0;
+
+    const char* units = " bytes";
+    double show = bytes;
+    if (tera > 1.0) {
+        show = tera;
+        units = " Tibytes";
+    } else if (giga > 1.0) {
+        show = giga;
+        units = " Gibytes";
+    } else if (mega > 1.0) {
+        show = mega;
+        units = " Mibytes";
+    } else if (kilo > 1.0) {
+        show = kilo;
+        units = " Kibytes";
+    }
+    prec++;
+    if (show >= 10.0)  prec++;
+    if (show >= 100.0)  prec++;
+
+    Put(show, 0, prec);
+    out() << units;
+}
+
+void outputStream::Pad(char repeat, int count)
+{
+    for (; count>0; count--) {
+        out().put(repeat);
+    }
+}
+
+void outputStream::PutHex(unsigned char data)
+{
+    out() << std::hex << std::setfill('0') << std::setw(2) << data << std::dec;
+}
+
+void outputStream::PutHex(unsigned data)
+{
+    out() << std::hex << std::setfill('0') << std::setw(8) << data << std::dec;
+}
+
+void outputStream::PutHex(unsigned long data)
+{
+    out() << std::hex << std::setfill('0') << std::setw(16) << data << std::dec;
+}
+
+void outputStream::PutInteger(const std::string &data, int width)
+{
+    static std::stringstream ss;
+
+    unsigned next_comma = data.length() % 3;
+
+    unsigned i;
+    if (data[0] == '-') {
+        ss << data[0] << data[1];
+        i=2;
+    } else {
+        ss << data[0];
+        i=1;
+    }
+    if (i>next_comma) next_comma += 3;
+
+    while (data[i]) {
+        if (i==next_comma) {
+            ss << thousands->getStr();
+            next_comma += 3;
+        }
+        ss << data[i];
+        i++;
+    } // while
+
+    out() << std::setw(width) << ss.str();
+}
+
+void outputStream::Put(double data, int width)
+{
+    out() << std::setw(width) << data;
+}
+
+void outputStream::Put(double data, int width, int prec)
+{
+    int oldprec = out().precision();
+    out().precision(prec);
+    out() << std::setw(width) << data;
+    out().precision(oldprec);
+}
+
+void outputStream::setRealFormat(unsigned rf)
+{
+    switch (rf) {
+        case 1:     out() << std::fixed;            return;
+        case 2:     out() << std::scientific;       return;
+        default:    out() << std::defaultfloat;     return;
     }
 }
 
--- src/Utils/outstream.h
+++ src/Utils/outstream.h
@@ -2,10 +2,21 @@
 #ifndef OUTSTREAM_H
 #define OUTSTREAM_H
 
+#include <string>
 #include <iostream>
 #include <fstream>
+#include <sstream>
+
+class shared_string;
 
 class outputStream {
+    public:
+        /*
+         * How to format real values
+         */
+        static const unsigned RF_GENERAL = 0;
+        static const unsigned RF_FIXED = 1;
+        static const unsigned RF_SCIENTIFIC = 2;
     public:
         outputStream(std::ostream &_deflt);
         virtual ~outputStream();
@@ -39,12 +50,43 @@ class outputStream {
         }
 
         //
-        // Custom put thingies here
+        // Repeat the given character
+        void Pad(char repeat, int count);
+
         //
+        // Custom put thingies here for convenience
+        //
+        void PutHex(unsigned char data);
+        void PutHex(unsigned int  data);
+        void PutHex(unsigned long data);
+        void PutMemoryCount(size_t bytes, int prec);
+
+        /// Makes comma-separated integers.
+        template <class T>
+        inline void PutInt(T data, int width=0)
+        {
+            static std::stringstream ss;
+            ss << data;
+            PutInteger(ss.str(), width);
+        }
+        void PutInteger(const std::string &integer, int width);
+
+        // For reals
+        void Put(double data, int width);
+        void Put(double data, int width, int prec);
+
+        //
+        // For options later
+        //
+        inline shared_string* & linkThousands() {
+            return thousands;
+        }
+        void setRealFormat(unsigned rf);
 
     private:
         std::ostream &deflt;
         std::ofstream fout;
+        shared_string* thousands;
 };
 
 #endif
