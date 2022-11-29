d0e03d646d83c692e7369d63fff1f2845fe8fec3
Andrew Miner <asminer@iastate.edu>
2022-11-29 00:27:08 -0600

    Forgot one

--- src/ExprLib/type.h
+++ src/ExprLib/type.h
@@ -8,7 +8,6 @@
 
 class result;
 class io_environ;
-// class OutputStream;
 
 // all of this required for infinity string option.
 // class option;
@@ -117,7 +116,7 @@ public:
         @param  s  Stream to write to.
         @param  r  Result to display.
   */
-  bool print(OutputStream &s, const result& r) const;
+  bool print(std::ostream &s, const result& r) const;
 
   /** Print a result of this type.
       We must be "printable" according to isPrintable().
@@ -125,7 +124,7 @@ public:
         @param  r  Result to display.
         @param  w  Width to use.
   */
-  bool print(OutputStream &s, const result& r, int w) const;
+  bool print(std::ostream &s, const result& r, int w) const;
 
   /** Print a result of this type.
       We must be "printable" according to isPrintable().
@@ -134,14 +133,14 @@ public:
         @param  w  Width to use.
         @param  p  Precision to use.
   */
-  bool print(OutputStream &s, const result& r, int w, int p) const;
+  bool print(std::ostream &s, const result& r, int w, int p) const;
 
   /** Show a result.
       Just like print(), except for strings:
       show() will give you "the string.\n",
       while print() will give you the string.
   */
-  void show(OutputStream &s, const result& r) const;
+  void show(std::ostream &s, const result& r) const;
 
   /** Fill the result, from a string.
       Works only for "simple" types.
@@ -169,10 +168,10 @@ public:
   virtual bool equals(const result &x, const result &y) const;
 
 protected:
-  virtual bool print_normal(OutputStream &s, const result& r) const;
-  virtual bool print_normal(OutputStream &s, const result& r, int w) const;
-  virtual bool print_normal(OutputStream &s, const result& r, int w, int p) const;
-  virtual void show_normal(OutputStream &s, const result& r) const;
+  virtual bool print_normal(std::ostream &s, const result& r) const;
+  virtual bool print_normal(std::ostream &s, const result& r, int w) const;
+  virtual bool print_normal(std::ostream &s, const result& r, int w, int p) const;
+  virtual void show_normal(std::ostream &s, const result& r) const;
   virtual void assign_normal(result& r, const char* s) const;
   virtual bool equals_normal(const result &x, const result &y) const;
 };
@@ -244,7 +243,7 @@ public:
   inline int Length() const {
     return nt;
   }
-  virtual bool Print(OutputStream &s, int) const;
+  virtual bool Print(std::ostream &s, int) const;
   virtual bool Equals(const shared_object *o) const;
 };
 
