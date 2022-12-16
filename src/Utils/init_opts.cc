60aaaaf641038c6dc2323f817a348239ffb0d511
Andrew Miner <asminer@iastate.edu>
2022-12-15 20:48:50 -0600

    Working on option initializers

--- src/Utils/init_opts.cc
+++ src/Utils/init_opts.cc
@@ -2,7 +2,26 @@
 #include "init_opts.h"
 #include "../Options/optman.h"
 
+// **********************************************************************
+// *                                                                    *
+// *                optman_initializer class and methods                *
+// *                                                                    *
+// **********************************************************************
+
 static option_manager* OM;
+
+/**
+    Build the global option manager.
+    Requires nothing.
+    Builds resource OM.
+*/
+class optman_initializer : public initializer {
+        unsigned handle;
+    public:
+        optman_initializer();
+    protected:
+        virtual void execute();
+};
 
 optman_initializer::optman_initializer()
     : initializer("optman_initializer", 1, 0)
@@ -23,7 +42,92 @@ void optman_initializer::execute()
 
 static optman_initializer _omi;
 
-//
+// **********************************************************************
+// *                                                                    *
+// *              checklist_initializer  class and methods              *
+// *                                                                    *
+// **********************************************************************
+
+/**
+    Build a checklist option for switchable messages.
+    These are options like "Report" and "Debug"
+    with lots of suboptions.
+
+    Requires resource OM.
+    Builds resource with name = optname.
+*/
+class checklist_initializer : public initializer {
+        const char* optname;
+        const char* optdoc;
+
+        unsigned om_handle;
+        unsigned option_handle;
+    public:
+        checklist_initializer(const char* name, const char* doc);
+    protected:
+        virtual void execute();
+};
+
+checklist_initializer::checklist_initializer(const char* name, const char* doc)
+    : initializer("checklist_initializer", 1, 1)
+{
+    optname = name;
+    optdoc = doc;
+
+    om_handle = needs_resource("OM");
+    option_handle = builds_resource(name);
+}
+
+void checklist_initializer::execute()
+{
+    option_manager* OM = dynamic_cast <option_manager*>
+        (get_needed_resource(om_handle));
+
+    if (0==OM) return;      // Error out here?
+
+    set_built_resource(option_handle, OM->addChecklistOption(optname, optdoc));
+}
+
+static checklist_initializer _report_init(
+    "Report",
+    "Switches to control what reports, if any, are written to the report stream."
+);
+
+static checklist_initializer _debug_init(
+    "Debug",
+    "Switches to control what low-level debugging information, if any, is written to the report stream."
+);
+
+static checklist_initializer _warning_init(
+    "Warning",
+    "Switches to control which warning messages are displayed and which are suppressed."
+);
 
 
+// **********************************************************************
+// *                                                                    *
+// *                 checklistgroup_initializer methods                 *
+// *                                                                    *
+// **********************************************************************
+
+checklistgroup_initializer::checklistgroup_initializer(const char* main,
+    const char* name, const char* doc)
+    : initializer("checklistgroup_initializer", 1, 1)
+{
+    mainopt = main;
+    groupname = name;
+    groupdoc = doc;
+
+    main_handle = needs_resource(main);
+    group_nahdle = builds_resource(name);
+}
+
+void checklistgroup_initializer::execute()
+{
+    option* main = dynamic_cast <option*> (get_needed_resource(main_handle));
+
+    if (0==main) return;      // Error out here?
+
+    set_built_resource(group_handle, main->addChecklistGroup(groupname, groupdoc));
+}
 
--- src/Utils/init_opts.h
+++ src/Utils/init_opts.h
@@ -5,56 +5,30 @@
 #include "messages.h"
 
 /**
-    Build the global option manager.
-    Requires nothing.
-    Builds resource OM.
-*/
-class optman_initializer : public initializer {
-        unsigned handle;
-    public:
-        optman_initializer();
-    protected:
-        virtual void execute();
-};
-
-
-/**
-    Build a checklist option for switchable messages.
-    These are options like "Report" and "Debug"
-    with lots of suboptions.
-
-    Requires resource OM.
-    Builds resource with name = optname.
-*/
-class checklist_initializer : public initializer {
-        const char* optname;
-        const char* optdoc;
-    public:
-        checklist_initializer(const char* name, const char* doc);
-    protected:
-        virtual void execute();
-};
-
-/**
-    Build a group of items, in a checklist option.
-
-    Requires resource OM.
-    Requires resource with name = mainopt.
-    Builds resource with name = groupname.
+    Initializer for a group of items, in a checklist option.
 */
 class checklistgroup_initializer : public initializer {
         const char* mainopt;
         const char* groupname;
         const char* groupdoc;
+
+        unsigned main_handle;
+        unsigned group_handle;
     public:
+        /**
+            Set up an initializer.
+                @param  main    Main checklist option name, e.g., "Warning"
+                @param  name    Group name, e.g., "pn_ALL"
+                @param  doc     Group documentation
+        */
         checklistgroup_initializer(const char* main, const char* name, const char* doc);
     protected:
         virtual void execute();
 };
 
+
 /**
-    Build and add the appropriate initializer(s) for a switchable message.
+    Initializer for a switchable message.
 */
-void addInitializer(switchable_msg &M);
 
 #endif
