9a553ba80ff875997364981c0d6a4001591bd783
Andrew Miner <asminer@iastate.edu>
2023-01-14 21:13:35 -0600

    Cleanup

--- src/ExprLib/symb_tab.h
+++ src/ExprLib/symb_tab.h
@@ -28,34 +28,10 @@ class symbol_table {
             return smart_cast <symbol*> (table.find(&CS));
         }
 
-        /// Remove the given symbol.  Return true if the item was in the table.
-        // bool removeSymbol(symbol* s);
-
         /// Traverse the symbol table
         inline void traverse(shared_visitor &t) const {
             table.traverse(t);
         }
-
-        /** Pop and return the last added symbol.
-            This only works if there is no chaining, i.e.,
-            symbol names are unique;
-            if this might not be the case, we return a null pointer.
-        */
-//        symbol* pop();
-
-        /** Return the ith symbol added.
-            Like pop(), this works only if there is no chaining.
-                @param  i  The symbol number to return.
-                @return 0, if i is out of range; a valid symbol, otherwise.
-        */
-        // symbol* getItem(unsigned i) const;
-
-        /** Grab a copy of all symbols, in order.
-            Used primarily for documentation.
-        */
-//        void copyToArray(const symbol** list);
-
- //       void documentSymbols(doc_formatter &df, const char* keyword);
 
         const splayOfShared& getTable() const { return table; }
 
@@ -96,48 +72,10 @@ class symbol_table {
             allModelsTable().addSymbol(s);
         }
 
-        /*
-    private:
-        /// The head of a list of symbols, with this name.
-        struct symbol_list : public shared_object {
-                /// Name of all symbols in this list.
-                const char* name;
-                /// Front of the list.
-                symbol* front;
-            public:
-                symbol_list();
-
-                // Required for shared_object
-                virtual bool Print(std::ostream &s, int width=0) const;
-                virtual int Compare(const shared_object* s) const;
-
-                void Fill(symbol* f);
-                void Fill(const char* n);
-        };
-
-    private:    // helpers
-        inline symbol_list* PopFree() {
-            if (!FreeList) return nullptr;
-            symbol_list* tmp = FreeList;
-            FreeList = (symbol_list*) FreeList->front;
-            return tmp;
-        }
-        inline symbol_list* NewList() {
-            symbol_list* tmp = PopFree();
-            if (!tmp) tmp = new symbol_list;
-            return tmp;
-        }
-        inline void RecycleList(symbol_list* f) {
-            f->front = (symbol*) FreeList;
-            FreeList = f;
-        }
-        */
-
     private:
         unsigned num_syms;
         unsigned num_names;
         splayOfShared table;
-        // symbol_list* FreeList;
         static symbol_table* _global;
         static symbol_table* _allModels;
 };
