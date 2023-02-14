16320f162b1c40a2b908a25ab3720e377338194b
Andrew Miner <asminer@iastate.edu>
2023-02-14 16:58:07 -0600

    Working on developer scripts

---
+++ Devel/PullMeddly.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! git subrepo --version 1> /dev/null 2> /dev/null; then
+  echo "This script needs git subrepo."
+  echo "To install, try the following."
+  echo "    git clone https://github.com/ingydotnet/git-subrepo /path/to/subrepo"
+  echo "    Run /path/to/subrepo/.rc in .bashrc or .zshrc"
+  echo
+  exit 1
+fi
+
+git subrepo pull _Meddly
