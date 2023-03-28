d90aa09cbf0659b577878fa3d98638ca9346e163
Andrew Miner <asminer@iastate.edu>
2023-03-27 23:22:40 -0500

    Script formatting

--- Devel/Release.sh
+++ Devel/Release.sh
@@ -5,13 +5,13 @@ LASISU="git@git.las.iastate.edu:asminer/Smart.git"
 GITHUB="git@github.com:asminer/smart.git"
 
 if [ $# -ne 0 ]; then
-  printf "\nUsage: $0 \n\n"
-  printf "Creates a release based on the version number in configure.ac.\n"
-  printf "Automatically updates the revision date in the source,\n"
-  printf "and creates a tag for the release, named v[version].\n\n"
-  printf "Before running, be sure that all modified files have been committed\n"
-  printf "to the repository.\n\n"
-  exit 0
+    printf "\nUsage: $0 \n\n"
+    printf "Creates a release based on the version number in configure.ac.\n"
+    printf "Automatically updates the revision date in the source,\n"
+    printf "and creates a tag for the release, named v[version].\n\n"
+    printf "Before running, be sure that all modified files have been\n"
+    printf "committed  to the repository.\n\n"
+    exit 0
 fi
 
 printf "Checking remotes...\n"
@@ -20,22 +20,22 @@ OREM=`git remote -v | awk '/origin.*(push)/ {print $2}'`
 GREM=`git remote -v | awk '/public-github.*(push)/ {print $2}'`
 
 if [ "$OREM" == "$LASISU" ]; then
-  echo "    Found origin from ISU"
+    echo "    Found origin from ISU"
 else
-  echo "    Missing: origin should be ISU repository"
-  exit 1
+    echo "    Missing: origin should be ISU repository"
+    exit 1
 fi
 
 if [ "$GREM" == "$GITHUB" ]; then
-  echo "    Found public-github"
+    echo "    Found public-github"
 else
-  echo "    Missing: public-github"
-  echo
-  echo "        To add this remote, run"
-  echo "            git remote add public-github $GITHUB"
-  echo "        and then re-run the script."
-  echo
-  exit 1
+    echo "    Missing: public-github"
+    echo
+    echo "        To add this remote, run"
+    echo "            git remote add public-github $GITHUB"
+    echo "        and then re-run the script."
+    echo
+    exit 1
 fi
 
 
