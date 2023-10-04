6f9d50815dc90d2cbdafe7e5ef686b35c94f9d28
Andrew Miner <asminer@iastate.edu>
2023-10-03 20:20:17 -0500

    Bugfix for aggregates

--- src/ExprLib/ops_misc.cc
+++ src/ExprLib/ops_misc.cc
@@ -87,8 +87,8 @@ protected:
 // ******************************************************************
 
 void_seq::void_seq(const char* fn, int ln, expr** x, int n)
- : assoc(fn, ln, exprman::aop_semi, em->VOID, x, n) 
-{ 
+ : assoc(fn, ln, exprman::aop_semi, em->VOID, x, n)
+{
 }
 
 bool void_seq::Print(OutputStream &s, int d) const
@@ -116,7 +116,7 @@ void void_seq::Compute(traverse_data &x)
   } // for i
 }
 
-expr* void_seq::buildAnother(expr **x, int n) const 
+expr* void_seq::buildAnother(expr **x, int n) const
 {
   return new void_seq(Filename(), Linenumber(), x, n);
 }
@@ -130,7 +130,7 @@ expr* void_seq::buildAnother(expr **x, int n) const
 class void_seq_op : public sequence_op {
 public:
   void_seq_op(const type* vtype);
-  virtual assoc* makeExpr(const char* fn, int ln, expr** list, 
+  virtual assoc* makeExpr(const char* fn, int ln, expr** list,
         bool* flip, int N) const;
 };
 
@@ -142,7 +142,7 @@ void_seq_op::void_seq_op(const type* vtype) : sequence_op(vtype)
 {
 }
 
-assoc* void_seq_op::makeExpr(const char* fn, int ln, expr** list, 
+assoc* void_seq_op::makeExpr(const char* fn, int ln, expr** list,
         bool* flip, int N) const
 {
   delete[] flip;
@@ -176,8 +176,8 @@ protected:
 // ******************************************************************
 
 next_state_seq::next_state_seq(const char* fn, int ln, expr** x, int n)
- : assoc(fn, ln, exprman::aop_semi, em->NEXT_STATE, x, n) 
-{ 
+ : assoc(fn, ln, exprman::aop_semi, em->NEXT_STATE, x, n)
+{
 }
 
 bool next_state_seq::Print(OutputStream &s, int d) const
@@ -211,12 +211,12 @@ void next_state_seq::Traverse(traverse_data &x)
   if (x.elist) {
     for (int i=0; i<opnd_count; i++) {
       x.elist->Append(operands[i]);
-    } 
+    }
   }
   x.answer->setInt(x.answer->getInt()+opnd_count);
 }
 
-expr* next_state_seq::buildAnother(expr **x, int n) const 
+expr* next_state_seq::buildAnother(expr **x, int n) const
 {
   return new next_state_seq(Filename(), Linenumber(), x, n);
 }
@@ -230,7 +230,7 @@ expr* next_state_seq::buildAnother(expr **x, int n) const
 class next_state_seq_op : public sequence_op {
 public:
   next_state_seq_op(const type* the_type);
-  virtual assoc* makeExpr(const char* fn, int ln, expr** list, 
+  virtual assoc* makeExpr(const char* fn, int ln, expr** list,
         bool* flip, int N) const;
 };
 
@@ -243,7 +243,7 @@ next_state_seq_op::next_state_seq_op(const type* the_type)
 {
 }
 
-assoc* next_state_seq_op::makeExpr(const char* fn, int ln, expr** list, 
+assoc* next_state_seq_op::makeExpr(const char* fn, int ln, expr** list,
         bool* flip, int N) const
 {
   delete[] flip;
@@ -262,11 +262,11 @@ assoc* next_state_seq_op::makeExpr(const char* fn, int ln, expr** list,
 // ******************************************************************
 
 /**   The class used to aggregate expressions.
- 
+
       We are derived from assoc, but most of the
       provided functions of assoc must be overloaded
       (because of the special nature of aggregates).
-*/  
+*/
 
 class aggregates : public assoc {
 public:
@@ -286,8 +286,8 @@ protected:
 // ******************************************************************
 
 aggregates::aggregates(const char* fn, int line, expr **x, int nc)
- : assoc (fn, line, exprman::aop_colon, (typelist*) 0, x, nc) 
-{ 
+ : assoc (fn, line, exprman::aop_colon, (typelist*) 0, x, nc)
+{
   DCASSERT(nc>0);
   // determine the type
   typelist* tl = new typelist(nc);
@@ -297,10 +297,10 @@ aggregates::aggregates(const char* fn, int line, expr **x, int nc)
   SetType(tl);
 }
 
-expr* aggregates::GetComponent(int i) 
-{ 
+expr* aggregates::GetComponent(int i)
+{
   CHECK_RANGE(0, i, opnd_count);
-  return operands[i]; 
+  return operands[i];
 }
 
 void aggregates::Compute(traverse_data &x)
@@ -315,6 +315,38 @@ void aggregates::Compute(traverse_data &x)
 
 void aggregates::Traverse(traverse_data &x)
 {
+  if (traverse_data::Substitute == x.which) {
+    expr** newargs = new expr* [opnd_count];
+    bool notequal = false;
+    for (int i=0; i<opnd_count; i++) {
+        if (operands[i]) {
+            operands[i]->Traverse(x);
+            newargs[i] = smart_cast <expr*> (Share(x.answer->getPtr()));
+        } else {
+            newargs[i] = nullptr;
+        }
+        if (newargs[i] != operands[i]) notequal = true;
+    } // for i
+    if (notequal) {
+        x.answer->setPtr(
+            new aggregates(
+                Filename(), Linenumber(), newargs, opnd_count
+            )
+        );
+    } else {
+        for (int i=0; i<opnd_count; i++) {
+            Delete(newargs[i]);
+        }
+        delete[] newargs;
+        x.answer->setPtr(Share(this));
+    }
+    return;
+  }
+
+  //
+  // All other traversals
+  //
+
   int i = x.aggregate;
   CHECK_RANGE(0, i, opnd_count);
   x.aggregate = 0;
@@ -355,7 +387,7 @@ public:
   virtual int getPromoteDistance(expr** list, bool* flip, int N) const;
   virtual int getPromoteDistance(bool f, const type* lt, const type* rt) const;
   virtual const type* getExprType(bool f, const type* l, const type* r) const;
-  virtual assoc* makeExpr(const char* fn, int ln, expr** list, 
+  virtual assoc* makeExpr(const char* fn, int ln, expr** list,
         bool* flip, int N) const;
 };
 
@@ -384,7 +416,7 @@ const type* aggreg_op::getExprType(bool f, const type* l, const type* r) const
   return 0;
 }
 
-assoc* aggreg_op::makeExpr(const char* fn, int ln, expr** list, 
+assoc* aggreg_op::makeExpr(const char* fn, int ln, expr** list,
         bool* flip, int N) const
 {
   delete[] flip;
